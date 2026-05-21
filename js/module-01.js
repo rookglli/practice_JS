4f2a7df5941658711edd6c94f1654d0086c75901
Roman Drok <rookglli@gmail.com>
2026-05-21 19:55:57 +0400

    add 1 day 1 modul

---
+++ js/module-01.js
@@ -0,0 +1,35 @@
+// const name = "Roman";
+// const age = 41;
+// const pet = false;
+
+// Створи змінну price зі значенням 150 і змінну discount зі значенням 20. Порахуй фінальну ціну і виведи результат.
+
+// const price = 150;
+// const discount = 20;
+// const dicVal = (price * discount) / 100;
+// const totalPrice = price - dicVal;
+
+// console.log(totalPrice);
+
+// Створи змінну з твоїм повним іменем.Виведи кількість символів у ньому.
+
+// const fullName = 'Roman Drok';
+
+// console.log (fullName.length);
+
+// Є рядок "javascript is awesome". Зроби велику першу літеру кожного слова.
+
+// const fullSting = 'javascript is awesome';
+// const firstWord = fullSting.slice(0, 10);
+// console.log(firstWord);
+
+// Створи змінну password = "myPass123". Перевір чи містить вона цифру "1" і чи починається з "my".
+// Виведи обидва результати
+
+// const password = 'myPass123';
+// const hasOne = password.includes('1');
+// const hasStart = password.startsWith('my');
+
+// console.log(password.endsWith('123'));
+// console.log(hasOne);
+// console.log(hasStart);
