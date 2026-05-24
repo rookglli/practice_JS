219ed14c211042229b68c610ef11807113c6a4dd
Roman Drok <rookglli@gmail.com>
2026-05-25 00:40:03 +0400

    module 2 method

--- js/module-02.js
+++ js/module-02.js
@@ -97,7 +97,117 @@
 //! end - кінець числового проміжку
 //! Використовуючи оператор &&, доповни функцію isNumberInRange таким чином, щоб вона повертала результат входження number у числовий проміжок від start до end включно. Тобто число повинно бути одночасно і більшим або дорівнювати start, і меншим або дорівнювати end. Результатом виклику функції має бути буль true або false.
 
-function isNumberInRange(start, end, number) {
-  return number >= start && number <= end;
-}
-console.log(isNumberInRange(0, 100, 57));
+// function isNumberInRange(start, end, number) {
+//   return number >= start && number <= end;
+// }
+// console.log(isNumberInRange(0, 100, 57));
+
+//! Функція checkAccess(subType) перевіряє, чи може користувач отримати доступ до контенту.Перевірка відбувається за типом передплати.Використовуючи оператор "АБО", доповни код функції так, щоб якщо значення параметра subType дорівнює рядкам "pro" або "vip", то функція повертала true і користувач отримував доступ.В іншому випадку повертала false.
+
+// function checkAccess(subType) {
+//   return subType === 'pro' || subType === 'vip';
+// }
+// console.log(checkAccess('vop'));
+
+//! Функція toggleModalVisibility(isVisible) перемикає стан відображення модального вікна. У параметрі isVisible вона очікує буль true або false. Доповни код функції так, щоб вона повертала протилежне значення цього параметра.
+
+// function toggleModalVisibility(isVisible) {
+//   return !isVisible;
+// }
+
+// console.log(toggleModalVisibility(false));
+
+//! Функція getSubstring(string, length) приймає рядок і повертає новий підрядок (його часткову копію). Вона оголошує два параметри, значення яких будуть задаватися під час її виклику:
+
+//! string - оригінальний рядок
+//! length - довжина нового підрядка
+//! Доповни код функції так, щоб вона повертала новий підрядок, який починається від початку string і має довжину length.
+
+// function getSubstring(string, length) {
+//   return string.slice(0, length);
+// }
+// console.log(getSubstring('Hello world', 3));
+
+//! Функція normalizeInput(input, to) оголошує два параметри:
+
+//! input - рядок, який потрібно привести до певного регістру
+//! to - рядок с двома можливими значеннями: "upper" або "lower", що вказують до якого регістру потрібно привести значення input
+//! Доповни код функції таким чином, щоб:
+
+//! Якщо значення параметра to — це рядок "upper", то функція повертала копію рядка input, але у верхньому регістрі
+//! В іншому разі, функція повертала копію рядка inputу нижньому регістрі
+
+// function normalizeInput(input, to) {
+//   const inputLower = input.toLowerCase();
+//   const inputUpper = input.toUpperCase();
+//   if (to === 'lower') {
+//     return inputLower;
+//   } else {
+//     return inputUpper;
+//   }
+// }
+// console.log(normalizeInput('Big SALE', 'upper'));
+
+//! Функція checkForName(fullName, firstName) приймає два параметри та повертає буль true або false - результат перевірки входження підрядка firstName у рядок fullName.
+
+//! fullName - рядок, що містить повне ім'я
+//! firstName - рядок, що містить ім'я для перевірки його входження в повне ім'я
+//! Доповни тіло функції таким чином, щоб вона повертала результат перевірки входження імені (параметр firstName), у повне ім'я (параметр fullName). Врахуй, що регістр символів в параметрах fullName і firstName наперед не відомий. Перед перевіркою входження не забудь прирівняти їх до однакового регістру.
+
+// function checkForName(fullName, firstName) {
+//   const fullNameRegister = fullName.toLowerCase();
+//   const firstNameRegister = firstName.toLowerCase();
+//   return fullNameRegister.includes(firstNameRegister);
+// }
+// console.log(checkForName('Jason Neis', 'Jacob'));
+
+//! Функція checkFileExtension(fileName, ext) приймає два параметри:
+
+//! fileName - рядок, що зберігає ім'я файлу з розширенням, наприклад, styles.css, hello.js тощо.
+//! ext - рядок, що зберігає розширення, наприклад, .css, .js тощо
+//! Доповни код функції так, щоб:
+
+//! Якщо ім'я файлу fileName закінчується розширенням, зазначеним у параметрі ext, то функція повертала рядок "File extension matches"
+//! В іншому разі, функція повертала рядок "File extension does not match"
+
+// function checkFileExtension(fileName, ext) {
+//   if (fileName.endsWith(ext)) {
+//     return `File extension matches`;
+//   } else {
+//     return `File extension does not match`;
+//   }
+// }
+
+// console.log(checkFileExtension('app.js', '.js'));
+
+//! Функція getFileName(file) приймає один параметр
+
+//! file - рядок з іменем файлу. Ім'я файлу може бути з розширенням, наприклад: styles.css, app.js або без, наприклад: styles, app.
+//! Використовуючи методи indexOf та slice, доповни код функції таким чином, щоб:
+
+//! Вона перевіряла наявність розширення в імені файлу (назва розширення відокремлюється від імені файла крапкою)
+//! Якщо ім'я файлу не містить розширення, то функція повертала новий підрядок, що містить ім'я файлу без змін
+//! В іншому разі функція повертала підрядок з іменем файлу, але без розширення
+
+// function getFileName(file) {
+//   const dotIndex = file.indexOf('.');
+
+//   if (dotIndex === -1) {
+//     return file;
+//   } else {
+//     return file.slice(0, dotIndex);
+//   }
+// }
+
+//! Функція createFileName(name, ext) приймає два параметри:
+
+//! name - рядок, що зберігає ім'я файлу без розширення, яке вводить користувач. Воно може містити зайві пробіли на початку або в кінці рядка, наприклад "order ", " finance " тощо
+//! ext - рядок, що зберігає розширення, наприклад "txt", "xml" тощо
+//! Використовуючи синтаксис шаблонних рядків і метод trim(), доповни код функції таким чином, щоб вона повертала повне (об'єднане) ім'я файлу з доданим розширенням, зазначеним у параметрі ext у форматі ім'я.розширення. Також повне ім'я файлу не повинно містити зайвих пробілів на початку або наприкінці.
+
+// function createFileName(name, ext) {
+//   const nameTrim = name.trim();
+//   return `${nameTrim}.${ext}`;
+// }
+
+// console.log(createFileName(' order ', 'txt'));
