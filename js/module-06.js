591bc8466ac82ffce847d25f9a605606aa901cc1
Roman Drok <rookglli@gmail.com>
2026-06-09 02:20:02 +0400

    done 3-4 task

--- js/module-06.js
+++ js/module-06.js
@@ -18,6 +18,51 @@
 
 // console.log(pizzaPalace.order('Smoked'));
 
+//! Зміни код таким чином, щоб об'єкт parent став прототипом для об'єкта у змінній сhild.
 
+// const parent = {
+//   name: 'Stacey',
+//   surname: 'Moore',
+//   age: 54,
+//   heritage: 'Irish',
+// };
 
+// const child = Object.create(parent);
+// child.name = 'Jason';
+// child.age = 27;
+
+//! Зміни код, побудувавши ланцюжок прототипів таким чином, щоб об'єкт ancestor був прототипом для parent, а той, своєю чергою, був прототипом для child.
+
+// const ancestor = {
+//   name: 'Paul',
+//   age: 83,
+//   surname: 'Dawson',
+//   heritage: 'Irish',
+// };
+
+// const parent = Object.create(ancestor);
+// parent.name = 'Stacey';
+// parent.surname = 'Moore';
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = 'Jason';
+// child.age = 27;
+
+//! Додай класу Car метод constructor, який приймає три параметри:
+
+//! brand - марка автомобіля
+//! model - модель автомобіля
+//! price - ціна автомобіля
+//! Клас Car повинен створювати об'єкт з одноіменними властивостями brand, model і price, які отримують свої значення з аргументів, переданих при створенні нового екземпляра за допомогою оператора new.
+
+// class Car {
+//   constructor(brand, model, price) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// }
+
+// const New = new Car();
 
